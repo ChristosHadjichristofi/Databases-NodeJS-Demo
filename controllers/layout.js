de3edb35c5e454024fb3f7bb9ea29118a08c6949
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2022-03-01 22:10:46 +0200

    Add Best Dribbler, Best Shooter cards

--- controllers/layout.js
+++ controllers/layout.js
@@ -1,5 +1,44 @@
+const { pool } = require('../utils/database');
+
 exports.getLanding = (req, res, next) => {
-    res.render('landing.ejs', {
-        pageTitle: "Landing Page"
+
+    let best_dribbling_grade, best_dribbler, best_shooting_grade, best_shooter;
+
+    pool.getConnection((err, conn) => {
+
+        let dribblingPromise = new Promise((resolve, reject) => {
+            conn.promise()
+            .query("SELECT * FROM grades, students WHERE course_name = 'DRI' AND grades.student_id = students.id ORDER BY grade DESC LIMIT 1")
+            .then(([rows, fields]) => {
+                best_dribbling_grade = rows[0].grade;
+                best_dribbler = rows[0].name + " " + rows[0].surname;
+                resolve();
+             })
+            .then(() => pool.releaseConnection(conn))
+            .catch(err => console.log(err))
+        })
+
+        let shootingPromise = new Promise((resolve, reject) => { 
+            conn.promise()
+            .query("SELECT * FROM grades, students WHERE course_name = 'SHO' AND grades.student_id = students.id ORDER BY grade DESC LIMIT 1")
+            .then(([rows, fields]) => {
+                best_shooting_grade = rows[0].grade;
+                best_shooter = rows[0].name + " " + rows[0].surname;
+                resolve();
+             })
+            .then(() => pool.releaseConnection(conn))
+            .catch(err => console.log(err))
+        })
+
+        Promise.all([dribblingPromise, shootingPromise]).then(() => {
+            res.render('landing.ejs', {
+                pageTitle: "Landing Page",
+                best_dribbling_grade: best_dribbling_grade,
+                best_dribbler: best_dribbler,
+                best_shooter: best_shooter,
+                best_shooting_grade: best_shooting_grade
+            })
+        });
+
     })
 }
